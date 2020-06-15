eef78d991042645e1bdec2b077fff923feebc549
Jatin Paliwal <jatinpaliwal181@gmail.com>
2020-06-15 11:51:54 +0530

    gridsome config alter

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
     use: '@gridsome/source-wordpress',
     options: {
-      baseUrl: 'http://f21a9b4f9de6.ngrok.io/', // required
+      baseUrl: 'http://f63b05684ec1.ngrok.io/', // required
       apiBase: 'wp-json',
       typeName: 'WordPress',
       perPage: 100,
